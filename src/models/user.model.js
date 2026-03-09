c4bb3b58226162bc668404580f738652d1e91bc8
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-03-09 08:13:08 +0530

    user model, controller and routes added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     email : {
         type : String,
         required : [true, "Email is required for creating a user"],
@@ -19,5 +20,33 @@ const userSchema = mongoose.Schema({
         required : [true, "Password is required for creating an account"],
         minlength : [6, 'password should contain more than 6 character'],
         select : false
-    },
+    }
+  },
+    {
+        timestamps : true
+    }
+)
+
+userSchema.pre("save", async function () {
+    if(!this.isModified("password")){
+        return
+    }
+
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash
+
+    return
+
 })
+
+userSchema.methods.comparePassword = async function (password) {
+    console.log(password, this.password)
+
+    return await bcrypt.compare(password, this.password)
+}
+
+
+const userModel = mongoose.model("user", userSchema)
+
+
+module.exports = userModel
