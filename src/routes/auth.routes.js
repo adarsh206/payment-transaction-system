59b230d702223c25277edb138ef6fe5f6661b8bb
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-03-10 06:56:43 +0530

    login api added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,6 +8,9 @@ const router = express.Router()
 // POST /api/auth/register
 router.post("/register", authController.userRegisterController)
 
+// POST /api/auth/login
+router.post("/login", authController.userLoginController)
+
 
 
 
