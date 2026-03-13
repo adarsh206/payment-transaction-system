a9d48f83ec7181284ac16647b3514332b9c91feb
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-03-13 07:35:34 +0530

    auth middleware added

--- src/app.js
+++ src/app.js
@@ -9,6 +9,19 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 
+
+/**
+ * - Routes required
+ */
+const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
+
+
+/**
+ * - Use Routes
+ */
+
 app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter)
 
 module.exports = app
