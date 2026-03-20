3b7899ff6b37dd05f316d7758976797b92e257ba
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-03-20 09:26:50 +0530

    logout routes and controller added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -12,6 +12,12 @@ router.post("/register", authController.userRegisterController)
 router.post("/login", authController.userLoginController)
 
 
+/**
+ * - POST /api/auth/logout
+ */
+router.post("/logout", authController.userLogoutController)
+
+
 
 
 module.exports = router
